fe02c3ddc040c191d98a9efe526230110eb62d83
thien <vophison299198@gmail.com>
2024-05-06 17:39:18 +0700

    update

--- src/components/layout/DefaulLayout/index.js
+++ src/components/layout/DefaulLayout/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header'
-import Sidebar from '../components/Sidebar'
+import Sidebar from '../DefaulLayout/Sidebar'
 import Styles from './DefaulLayout.module.scss'
 import classNames from 'classnames/bind'
 
