1f42936adfb6bb4bae9997ac23cf3bfdc39da936
thien <vophison299198@gmail.com>
2024-05-06 19:16:46 +0700

    update

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images ={
+    logo: require('../images/logo.svg' ).default
+
+}
+
+export default images
--- src/components/layout/components/header/index.js
+++ src/components/layout/components/header/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Classnames from 'classnames/bind'
 import styles from './header.module.scss';
+import images from '../../../../assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+import { faCircleXmark , faSpinner , faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = Classnames.bind(styles)
+
+
 
  function Header() {
     
@@ -10,7 +17,23 @@ const cx = Classnames.bind(styles)
  
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                  <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                  <input placeholder='Search account and videos' spellCheck={false}></input>
+                  <button className={cx('clear')}>{
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+               
+                  }</button>
+                           <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
 
+                  <button className={cx('search-btn')}>{
+           <FontAwesomeIcon className={cx('loading')} icon={faMagnifyingGlass}/>
+
+                  } </button>
+
+                </div>
         </div>
 
     </header>;
